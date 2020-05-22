daca94d357e0c277ac3b2758b2cdcd0a643cd6fc
Mohammed-Q <mohammed-q@users.noreply.github.com>
2020-05-22 05:24:46 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,45 +6,71 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+/*
+   I got the ul element and then got the child elements of the ul which is li.
+   I stored the number of students per page in the itemNumber variable. 
+*/
+const ul = document.querySelector('.student-list')
+const students = ul.children;
+const length = students.length;
+const itemNumber = 10;
 
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
+/*
+   Created a function called showPage that holds two parameter list and page.
+   List holds the student list and page holds the page number.
+   I created two variable that store the students to be display based on the page link clicked on. 
+   I created a loop to display the students.
+   I hid all students and then only displayed the students that correspond to the page number. 
+*/
+function showPage(list, page) {
+   const startIndex = (page * itemNumber) - itemNumber;
+   const endIndex = page * itemNumber;
+   for(let i = 0; i < length; i++) {
+      list[i].style.display = 'none'
+      if(i >= startIndex && i < endIndex) {
+         list[i].style.display = 'block'
+      }
+   }
+}
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 
+function appendPageLinks(list) {
+   const mainDiv = document.querySelector('.page');
+   let div = document.createElement('div');
+   div.className += 'pagination';
+   let ul = document.createElement('ul')
+   div.appendChild(ul)
+   const pages = Math.ceil(list.length/10);
+   for(let i = 1; i <= pages; i++) {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      if(i === 1) {
+         a.className = 'active';
+      }
+      a.href += '#'
+      a.innerHTML = i
+      li.appendChild(a);
+      ul.appendChild(li);
+      a.addEventListener('click', () => {
+         const active = document.querySelectorAll('.active');
+         event.target.className = 'active';
+         for(let j = 0; j < active.length; j++) { 
+            active[j].className = '';
+         }
+         showPage(students, i);
+      })
+   }
+   mainDiv.appendChild(div);
+}
 
+/*
+   I called the showPage function and displayed the first page of students.
+   Also, called the appendPageLinks functions which display all the page links.  
+*/
+showPage(students, 1);
+appendPageLinks(students);
 
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
